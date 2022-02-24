dc38317a5b83a8cdda2c56becce747d8a0f866a4
YeonV <trusted.yeon@gmail.com>
2022-02-24 13:33:09 +0100

    class to functional component

--- renderer/components/AudioContainer.jsx
+++ renderer/components/AudioContainer.jsx
@@ -1,109 +1,130 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import Visualizer from './Visualizer';
 
-class AudioDataContainer extends React.Component {
+const AudioDataContainer = ({ audioDeviceId, fft, bandCount }) => {
+  const [frequencyBandArray] = useState([...Array(bandCount).keys()]);
+  const audioData = useRef(null);
+  const audioContext = useRef(new AudioContext());
+  const theStream = useRef(null);
+  const theGain = useRef(null);
 
-    constructor(props) {
-        super(props);
-        this.state = {}
-        this.frequencyBandArray = [...Array(props.bandCount).keys()]
+  const initializeAudioAnalyser = () => {
+    getMedia(audioDeviceId).then((stream) => {
+      stream.current = stream;
+      if (!audioContext.current || audioContext.current.state === 'closed') {
+        return;
+      }
+      const source = audioContext.current.createMediaStreamSource(stream);
+      const analyser = audioContext.current.createAnalyser();
+      analyser.fftSize = fft;
+      const gain = audioContext.current.createGain();
+      theGain.current = gain.gain;
+      source.connect(gain);
+      gain.connect(analyser);
+      audioData.current = analyser;
+    });
+  };
+
+  const getFrequencyData = (styleAdjuster) => {
+    if (!audioData.current) {
+      return;
     }
+    const bufferLength = audioData.current.frequencyBinCount;
+    const amplitudeArray = new Uint8Array(bufferLength);
 
-    audioContext = new AudioContext();
-    theGain = null
-    theStream = null
+    audioData.current.getByteFrequencyData(amplitudeArray);
+    styleAdjuster(amplitudeArray);
+  };
 
-    initializeAudioAnalyser = () => {
-        this.getMedia(this.props.audioDeviceId).then(stream => {
-            this.theStream = stream
-            if (!this.audioContext || this.audioContext.state === 'closed') {
-                return
+  const getMedia = async (clientDevice) => {
+    const ad = await navigator.mediaDevices
+      .enumerateDevices()
+      .then((devices) =>
+        !!(
+          clientDevice !== null &&
+          devices.find((d) => d.deviceId === clientDevice)
+        )
+          ? clientDevice
+          : null
+      );
+    if (ad) {
+      try {
+        return await navigator.mediaDevices.getUserMedia({
+          audio: { deviceId: { exact: ad } },
+          video: false,
+        });
+      } catch (err) {
+        console.log('Error:', err);
+      }
+    } else {
+      try {
+        return await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: false,
+        });
+      } catch (err) {
+        console.log('Error:', err);
+      }
+    }
+  };
+
+  return (
+    <div style={{ height: 255, position: 'relative' }}>
+      <Visualizer
+        fft={fft}
+        bandCount={bandCount}
+        key={bandCount}
+        initializeAudioAnalyser={initializeAudioAnalyser}
+        audioContext={audioContext.current}
+        frequencyBandArray={frequencyBandArray}
+        getFrequencyData={getFrequencyData}
+        refresh={() => {
+          if (
+            audioContext.current &&
+            audioContext.current.state === 'running'
+          ) {
+            audioContext.current.state !== 'closed' &&
+              theStream.current &&
+              theStream.current.getTracks().forEach((track) => track.stop());
+
+            audioContext.current.state !== 'closed' &&
+              audioContext.current.suspend();
+
+            audioContext.current.state !== 'closed' &&
+              audioContext.current.resume();
+            audioData.current = null;
+          }
+        }}
+        stop={() => {
+          if (
+            audioContext.current &&
+            audioContext.current.state === 'running'
+          ) {
+            if (theGain.current) {
+              theGain.current.value = 0;
             }
-            const source = this.audioContext.createMediaStreamSource(stream);
-            const analyser = this.audioContext.createAnalyser();            
-            analyser.fftSize = this.props.fft;
-            const gain = this.audioContext.createGain()
-            this.theGain = gain.gain
-            source.connect(gain)
-            gain.connect(analyser)
-            this.setState({
-                audioData: analyser
-            })
-        })
-    }    
+            setTimeout(() => {
+              if (audioContext.current) {
+                audioContext.current.state !== 'closed' &&
+                  theStream.current &&
+                  theStream.current
+                    .getTracks()
+                    .forEach((track) => track.stop());
 
-    getFrequencyData = (styleAdjuster) => {
-        const bufferLength = this.state.audioData && this.state.audioData.frequencyBinCount;
-        const amplitudeArray = new Uint8Array(bufferLength);
-        this.state.audioData && this.state.audioData.getByteFrequencyData(amplitudeArray)
-        styleAdjuster(amplitudeArray)
-    }
-    getMedia = async (clientDevice) => {
-        const ad = await navigator.mediaDevices.enumerateDevices()
-            .then((devices) => !!(clientDevice !== null && devices.find(d => d.deviceId === clientDevice))
-                ? clientDevice
-                : null)
-        if (ad) {
-            try {
-                return await navigator.mediaDevices.getUserMedia({
-                    audio: { deviceId: { exact: ad } },
-                    video: false,
-                })
-            } catch (err) {
-                console.log('Error:', err)
-            }
-        } else {
-            try {
-                return await navigator.mediaDevices.getUserMedia({
-                    audio: true,
-                    video: false,
-                })
-            } catch (err) {
-                console.log('Error:', err)
-            }
-        }
-    }
-    render() {
+                audioContext.current.state !== 'closed' &&
+                  audioContext.current.suspend();
 
-        return (
-            <div style={{ height: 255, position: 'relative' }}>
-                <Visualizer
-                    fft={this.props.fft}
-                    bandCount={this.props.bandCount}
-                    key={this.props.bandCount}
-                    initializeAudioAnalyser={this.initializeAudioAnalyser}
-                    audioContext={this.audioContext}
-                    frequencyBandArray={[...Array(this.props.bandCount).keys()]}
-                    getFrequencyData={this.getFrequencyData}
-                    refresh={() => {
-                        if (this.audioContext && this.audioContext.state === 'running') {
-                            this.audioContext.state !== 'closed' && this.theStream && this.theStream.getTracks().forEach(track => track.stop())
-                            this.audioContext && this.audioContext.state !== 'closed' && this.audioContext.suspend()
-                            this.audioContext && this.audioContext.state !== 'closed' && this.audioContext.resume()
-                            this.setState({
-                                audioData: null
-                            })
-                        }
-                    }}
-                    stop={() => {
-                        if (this.audioContext && this.audioContext.state === 'running') {
-                            if (this.theGain) {
-                                this.theGain.value = 0
-                            }
-                            setTimeout(() => {
-                                this.audioContext.state !== 'closed' && this.theStream && this.theStream.getTracks().forEach(track => track.stop())
-                                this.audioContext && this.audioContext.state !== 'closed' && this.audioContext.suspend()
-                                this.audioContext && this.audioContext.state !== 'closed' && this.audioContext.resume()
-                                this.setState({
-                                    audioData: null
-                                })
-                            }, 800)
-                        }
-                    }}
-                />
-            </div>
-        );
-    }
-}
+                audioContext.current.state !== 'closed' &&
+                  audioContext.current.resume();
+
+                audioData.current = null;
+              }
+            }, 800);
+          }
+        }}
+      />
+    </div>
+  );
+};
 
 export default AudioDataContainer;
